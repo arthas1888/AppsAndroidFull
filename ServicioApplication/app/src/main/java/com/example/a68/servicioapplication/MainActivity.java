1e3de2dd3533f5ce76cf312118b2ab8f2a97a492
arthas1988 <gustavo.morales@aulamatriz.edu.co>
2017-08-09 21:02:50 -0500

    service application

--- ServicioApplication/app/src/main/java/com/example/a68/servicioapplication/MainActivity.java
+++ ServicioApplication/app/src/main/java/com/example/a68/servicioapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a68.servicioapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.a68.servicioapplication.services.MyService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
 
     @Override
--- ServicioApplication/app/src/main/java/com/example/a68/servicioapplication/services/MyService.java
+++ ServicioApplication/app/src/main/java/com/example/a68/servicioapplication/services/MyService.java
@@ -1,10 +1,21 @@
 package com.example.a68.servicioapplication.services;
 
+import android.app.ActivityManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.util.Log;
 
 public class MyService extends Service {
+
+    private static final String TAG = MyService.class.getSimpleName();
+    private Handler handler;
+    private Runnable runnable;
+    private int count;
+
     public MyService() {
     }
 
@@ -12,5 +23,60 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        launchThread();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        count = 0;
+        handler = new Handler();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+
+    private void launchThread() {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                new MemmoryAsyncTask().execute();
+                handler.postDelayed(this, 1000);
+            }
+        };
+        runnable.run();
+    }
+
+    class MemmoryAsyncTask extends AsyncTask<Void, Void, String>{
+
+        private ActivityManager activityManager;
+        private ActivityManager.MemoryInfo memoryInfo;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            memoryInfo = new ActivityManager.MemoryInfo();
+            activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            activityManager.getMemoryInfo(memoryInfo);
+            return memoryInfo.availMem / 1024 + " KB";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "Memoria disponible: " + s);
+        }
     }
 }
